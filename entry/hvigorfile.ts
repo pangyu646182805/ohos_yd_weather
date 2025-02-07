895a754ea3c23d983c1f5ea530fc4fa0a6275a59
pangyu <yemz@bianwa.com>
2025-02-07 16:23:48 +0800

    添加zrouter依赖 开屏页面完成

--- entry/hvigorfile.ts
+++ entry/hvigorfile.ts
@@ -1,6 +1,15 @@
 import { hapTasks } from '@ohos/hvigor-ohos-plugin';
+import { routerRegisterPlugin, PluginConfig } from 'router-register-plugin'
+
+const config: PluginConfig = {
+  scanDirs: ['src/main/ets/pages', 'src/main/ets/views'], // 扫描的目录，如果不设置，默认是扫描src/main/ets目录
+  logEnabled: true, // 查看日志
+  viewNodeInfo: false, // 查看节点信息
+  isAutoDeleteHistoryFiles: true, // 删除无用编译产物
+  lifecycleObserverAttributeName: 'lifecycleObserver' // 可选，设置全局的生命周期实现类在组件上的属性名，默认值是lifecycleObserver
+}
 
 export default {
-    system: hapTasks,  /* Built-in plugin of Hvigor. It cannot be modified. */
-    plugins:[]         /* Custom plugin to extend the functionality of Hvigor. */
+  system: hapTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
+  plugins: [routerRegisterPlugin(config)]         /* Custom plugin to extend the functionality of Hvigor. */
 }
